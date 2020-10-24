287b6f5cf501a548e47d4a406e4ba7cda43c005c
ereussow <ereussow@ufl.edu>
2020-10-24 19:17:14 -0400

    Update sensor_test.ino

--- sensor_test/sensor_test.ino
+++ sensor_test/sensor_test.ino
@@ -8,13 +8,14 @@ void setup() {
 void loop() {
 
   digitalWrite(switchPin, LOW);
-  //Turns off LED because voltage going into Mosfet is 0V.
-  //This is below the gate threshold voltage
+  //Turns on LED because Mosfet open (Current goes through LED).
+  //This is below the gate threshold voltage.
   delay(1000);
-  //How long LED is off
+  //How long LED is on.
 
   digitalWrite(switchPin, HIGH);
-  //Turns on LED because voltage going into Mosfet is 5V.
+  //Turns of LED because voltage going into Mosfet is 5V 
+  //Connects resistor to ground (no current through LED)
   //This is above the gate threshold voltage (4V)
   delay(1000); 
   //How long LED is on
