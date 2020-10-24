5700db2792950b340d2918e40a9e7871c27f0aa7
ereussow <ereussow@ufl.edu>
2020-10-24 18:28:55 -0400

    Create sensor_test.ino

---
+++ sensor_test.ino
@@ -0,0 +1,21 @@
+const int switchPin = 10; //Digital Pin Controlling Mosfet Voltage
+
+void setup() {
+  pinMode(switchPin, OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(switchPin, LOW);
+  //Turns off LED because voltage going into Mosfet is 0V.
+  //This is below the gate threshold voltage 
+  //How long LED is off
+
+  digitalWrite(switchPin, HIGH);
+  //Turns on LED because voltage going into Mosfet is 5V.
+  //This is above the gate threshold voltage (4V)
+  delay(1000); 
+  //How long LED is on
+
+}
