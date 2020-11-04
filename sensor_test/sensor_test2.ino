f0d24d90af4b1805d5b9d20b0c0b23c785ed0e9a
J Combes <jcombes9834@gmail.com>
2020-11-03 20:41:49 -0500

    Updated function to allow for three LED's to be driven and added a few comment lines

--- sensor_test/sensor_test2.ino
+++ sensor_test/sensor_test2.ino
@@ -1,11 +1,14 @@
-const int GreenLEDPin = 10;
+const int GreenLEDPin = 10; //define which control signal is connected to a specific LED
 const int RedLEDPin = 11;
+const int BlueLEDPin = 12;
 
 void setup() {
   pinMode(GreenLEDPin, OUTPUT);
   pinMode(RedLEDPin, OUTPUT);
-  digitalWrite(RedLEDPin, LOW);
+  pinMode(BlueLEDPin, OUTPUT);
+  digitalWrite(RedLEDPin, LOW); //The LED's are wired 'active high', meaning a 'high' will turn the LED on
   digitalWrite(GreenLEDPin, LOW);
+  digitalWrite(BlueLEDPin, LOW);
 
 }
 
@@ -18,6 +21,11 @@ void loop() {
   digitalWrite(GreenLEDPin, HIGH);//Green LED turns on.
   delay(200);
   digitalWrite(GreenLEDPin, LOW);//Green LED turns off.
+  delay(800);
+
+  digitalWrite(GreenLEDPin, HIGH);//Blue LED turns on.
+  delay(200);
+  digitalWrite(GreenLEDPin, LOW);//Blue LED turns off.
   delay(800);
 
 }
